5f3d32d5f3e3303af80b4e1fd605449d29607088
Debanshu Samantaray <42534802+Debanshusam@users.noreply.github.com>
2024-11-20 10:31:08 +0530

    WIP FIX-3 Final: Consuming multiple commands separted by '\n'

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import socket  # noqa: F401
 # from loguru import logger
+import logging as logger
 
 
 def _encode_string_as_resp2(string: str) -> bytes:
@@ -17,50 +18,120 @@ def _encode_string_as_resp2(string: str) -> bytes:
     return resp_string.encode(encoding='utf-8')
 
 
-def _decode_resp_to_string(resp_string: bytes) -> str:
-    string_data: str = resp_string.decode(encoding="utf-8")
-    # to exclude the begining special char
-    stripped_str: str = str(string_data[1:])
-    return stripped_str
+def _extract_cmd_resp_bulk_array(resp_bulk_array: bytes) -> list[str]:
+
+    _resp_to_str: str = resp_bulk_array.decode("utf-8")
+    _commands: list[str] = _resp_to_str.splitlines()
+    _commands_copy: list[str] = _commands[::]
+
+    logger.debug(_commands_copy)
+
+    for ele in _commands:
+        if ele.startswith(("$", "*")):
+            _commands_copy.remove(ele)
+
+    return _commands_copy
 
 
-def _ping_pong_implementation(data: bytes) -> bytes:
-    return _encode_string_as_resp2(string="PONG")
+def _decode_resp_to_string(resp_string: bytes) -> str | list[str]:
+    """
+    Decodes a simple RESP string into a normal string.
+
+    Args:
+        resp_string: The RESP string to decode.
+
+    Returns:
+        The decoded string.
+    """
+
+    RESP_SEP: bytes = b'\r\n'
+
+    if resp_string.startswith(b'$'):
+
+        # Bulk string
+        logger.debug(resp_string[1:].split(RESP_SEP)[0])
+        length = int(resp_string[1:].split(RESP_SEP)[0])
 
 
-def _receive_delimited_message(sock: socket.socket, delimiter=b'\n') -> None:
-    whole_data: bytes = b''
-    while True:
-        part: bytes = sock.recv(1024)
-        whole_data += part
+        return resp_string[1:].split(RESP_SEP)[1][:length].decode('utf-8')
 
-        for each_cmd in whole_data.split(delimiter):
-            # Generating the response data
-            # Preserving the byte ending \n which is lost in split()
-            respsonse_data: bytes = _redis_cmd_router(
-                data=each_cmd + b"\n"
-                )
-            print(f"respsonse_data = {respsonse_data}")
-            # Write the same data back
-            sock.sendall(respsonse_data)
+    elif resp_string.startswith(b'+'):
+        # Simple string
+        return resp_string[1:].strip().decode('utf-8')
+
+    elif resp_string.startswith(b'*'):
+        """RESP Bulk Array
+        *<number-of-elements>\r\n<element-1>...<element-n>
+        ref: https://redis.io/docs/latest/develop/reference/protocol-spec/#arrays
+        """
+
+        return _extract_cmd_resp_bulk_array(resp_bulk_array=resp_string)
+
+    else:
+        raise ValueError("Unsupported RESP type")
 
 
-def _redis_cmd_router(data: bytes) -> bytes:
+def _ping_implementation() -> str:
+    # _repsonse: bytes = _encode_string_as_resp2(string="PONG")
+
+    _repsonse: str = "PONG"
+
+    return _repsonse
+
+
+def _redis_cmd_router(data: str) -> str:
 
     # PING-PONG cmd
-    text_data: str = _decode_resp_to_string(resp_string=data)
+    # text_data: str = _decode_resp_to_string(resp_string=data)
+    text_data: str = data
+
     if "ping" in text_data.lower():
-        print(f"PING Command detected: {text_data}")
-        return _ping_pong_implementation(data=data)
+        logger.info(f"PING Command detected: {text_data}")
+        return _ping_implementation()
     else:
         # returning back the input data
+        # return _encode_string_as_resp2(string=data)
         return data
+
+
+def _receive_client_input(sock_conn: socket.socket) -> None:
+    while True:
+        buffered_data: bytes = sock_conn.recv(1024)
+        logger.debug(f"buffered_data = {buffered_data}")
+
+        buffered_data_str: str = buffered_data.decode()
+        logger.debug(f"buffered_data_str = {buffered_data_str}")
+
+        if not buffered_data_str:
+            logger.warning("Empty data received from client")
+            break
+
+        client_input_cmd: str | list[str] = _decode_resp_to_string(buffered_data)
+
+        _ready_client_response(sock_conn, client_input_cmd)
+
+
+def _ready_client_response(sock_conn: socket.socket, client_input_cmd: str | list[str]):
+
+    respsonse_data: str = ""
+    if isinstance(client_input_cmd, str):
+        respsonse_data = _redis_cmd_router(client_input_cmd)
+        # Write the same data back
+        sock_conn.sendall(_encode_string_as_resp2(respsonse_data))
+
+    else:
+        for cmd in client_input_cmd:
+            respsonse_data = _redis_cmd_router(cmd)
+            logger.debug(f"respsonse_data = {respsonse_data}")
+
+            # Write the same data back
+            sock_conn.sendall(_encode_string_as_resp2(respsonse_data))
 
 
 def main():
     # You can use print statements as follows for debugging, t
     # hey'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    logger.info("Logs from your program will appear here!")
     # logger.info("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
@@ -83,10 +154,7 @@ def main():
         # Read data
         # data: bytes = connection.recv(1024)
 
-        _receive_delimited_message(
-            sock=connection,
-            delimiter=b"\n"
-        )
+        _receive_client_input(sock_conn=connection)
 
 
 if __name__ == "__main__":
