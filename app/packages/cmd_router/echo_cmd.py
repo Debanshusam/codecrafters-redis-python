2f26f537294f556bda985e33a576e142e36e4f78
Debanshu Samantaray <42534802+Debanshusam@users.noreply.github.com>
2024-11-30 16:13:32 +0530

    fix: added some updates to echo command implementation

--- app/packages/cmd_router/echo_cmd.py
+++ app/packages/cmd_router/echo_cmd.py
@@ -1,6 +1,17 @@
 import logging as logger
 
+from app.packages.resp_handler import resp_converter as RESPENC
 
-def echo_implementation(payload: bytes) -> bytes:
-    logger.debug(f"payload = {payload}")
-    return payload
+
+def echo_impl(payload: bytes) -> bytes:
+    payload_splited: list[bytes] = payload.split(RESPENC.RESP_BASE_SEP)
+    _response_payload: bytes = b""
+
+    payload_splited = [item for item in payload_splited if item]
+    logger.debug(f"payload_splited = {payload_splited}")
+
+    for x in payload_splited:
+        _response_payload += x
+
+    logger.debug(f"_response_payload = {_response_payload}")
+    return _response_payload
